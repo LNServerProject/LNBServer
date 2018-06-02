56714af93b8f11679840e6cbf794abc772adcd05
NotKyon <nocannedmeat@gmail.com>
2018-06-02 02:41:26 -0700

    Initial commit.

---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LNBServer.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Subtitle { get; set; }
+        public string MainParagraph { get; set; }
+
+        public void OnGet()
+        {
+            Subtitle = "LNB: Light Novel Tracking Project";
+            MainParagraph = "A project to make tracking your light novels easier and better than sites like Kitsu and MAL do.";
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LNBServer.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "(TO-DO: Grab four random LNs and present them. Also a lot of other things.)";
+        }
+    }
+}
